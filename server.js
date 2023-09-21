7023557359acd9242d8f1c05258de9e7e86ab4ab
alexneres <hello@alexneres.dev>
2023-09-21 20:18:19 -0300

    feat: handling get requests at /api/v1/tours/

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+const fs = require('fs')
 const express = require('express')
+const path = require('path')
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!' })
-})
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '/dev-data/data/tours-simple.json')),
+)
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...')
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: tours })
 })
 
 app.listen(port, () => {
