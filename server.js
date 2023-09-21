7cff46779dc92704db637f6976729c728658578b
alexneres <hello@alexneres.dev>
2023-09-21 20:57:33 -0300

    feat: handling post requests at /tours

--- server.js
+++ server.js
@@ -6,14 +6,33 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
-const tours = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '/dev-data/data/tours-simple.json')),
-)
+app.use(express.json())
+
+const dataFile = path.join(__dirname, '/dev-data/data/tours-simple.json')
+
+const tours = JSON.parse(fs.readFileSync(dataFile))
 
 app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: tours })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  tours.push(newTour)
+  fs.writeFile(dataFile, JSON.stringify(tours), (error) => {
+    console.error(error)
+  })
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: req.body,
+    },
+  })
 })
 
 app.listen(port, () => {
