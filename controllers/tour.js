9f25407ab7b46835d299730b3883155de7c96207
alexneres <hello@alexneres.dev>
2023-09-22 04:59:42 -0300

    feat: add advanced filtering to /tours

--- controllers/tour.js
+++ controllers/tour.js
@@ -5,8 +5,10 @@ const getAllTours = async (req, res) => {
     const queryObj = { ...req.query }
     const excludedFields = ['page', 'sort', 'limit', 'fields']
     excludedFields.forEach((el) => delete queryObj[el])
-    const query = Tour.find(queryObj)
 
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+    const query = Tour.find(JSON.parse(queryStr))
     const tours = await query
 
     res.status(200).json({
